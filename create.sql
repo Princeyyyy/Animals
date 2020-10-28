88670bba4969a214f12d0570dec793178374ae97
Princeyyyy <princemumo80@gmail.com>
2020-10-29 00:16:10 +0300

    added database query

---
+++ create.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE wildlife;
+
+\c wildlife
+
+CREATE TABLE sightings(
+    id SERIAL PRIMARY KEY,
+    animalName VARCHAR,
+    animalAge INTEGER,
+    animalHealth VARCHAR,
+    rangerId INTEGER,
+    type VARCHAR,
+    locationId INTEGER,
+    createdAt TIMESTAMP
+);
+
+CREATE TABLE rangers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    badgeNo INTEGER NOT NULL,
+    contact VARCHAR NOT NULL
+);
+
+CREATE TABLE locations(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
